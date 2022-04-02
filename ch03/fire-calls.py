3c5f954df36dd41d7a544b679c9b12aef1c5e71e
Stephen Moss <bladexr8@gmail.com>
2022-04-02 18:02:43 +1100

    finished ch03

--- ch03/fire-calls.py
+++ ch03/fire-calls.py
@@ -39,3 +39,50 @@ if __name__ == "__main__":
         .where(col("CallType").isNotNull())
         .agg(countDistinct("CallType").alias("DistinctCallTypes"))
         .show())
+
+    # manually rename a column and show calls where
+    # response times were > 5 minutes
+    new_fire_df = fire_df.withColumnRenamed("Delay", "ResponseDelayedMins")
+    (new_fire_df
+        .select("ResponseDelayedMins")
+        .where(col("ResponseDelayedMins") > 5)
+        .show(5, False))
+
+    # convert columns using sparks date/time functions
+    fire_ts_df = (new_fire_df
+        .withColumn("IncidentDate", to_timestamp(col("CallDate"), "dd/MM/yyyy"))
+        .drop("CallDate")
+        .withColumn("OnWatchDate", to_timestamp(col("WatchDate"), "dd/MM/yyyy"))
+        .drop("WatchDate")
+        .withColumn("AvailableDtTS", to_timestamp(col("AvailableDtTm"), "dd/MM/yyyy hh:mm:ss a"))
+        .drop("AvailableDtTm"))
+
+    # Select the converted columns
+    (fire_ts_df
+        .select("IncidentDate", "OnWatchDate", "AvailableDtTS")
+        .show(5, False))
+
+    # how many years of calls?
+    (fire_ts_df
+        .select(year('IncidentDate'))
+        .distinct()
+        .orderBy(year('IncidentDate'))
+        .show())
+
+    # find most common types of calls
+    (fire_ts_df
+        .select("CallType")
+        .where(col("CallType").isNotNull())
+        .groupBy("CallType")
+        .count()
+        .orderBy("count", ascending=False)
+        .show(n=10, truncate=False))
+
+    # basic statistics
+    # 1. sum of alarms
+    # 2. average response time
+    # 3. minimum and maximum response times
+    (fire_ts_df
+        .select(sum("NumAlarms"), avg("ResponseDelayedMins"),
+            min("ResponseDelayedMins"), max("ResponseDelayedMins"))
+            .show())
